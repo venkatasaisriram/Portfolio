69a24083240619ac7581f39a0435c9acfa988a0f
Chitrada Venkata Sai Sriram <133555263+venkatasaisriram@users.noreply.github.com>
2025-04-05 15:12:06 +0530

    Added Some Features
    1.Clicking on skills will navigate to projects section. 
    2.Clicking on certificates will pop up and show full view.
    3. Clicking submit button will redirect to mail.

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const gridCards2 = document.getElementById("grid-card2")
 const gridCards3 = document.getElementById("grid-card3")
 const gridCards4 = document.getElementById("grid-card4")
 const sectionProject = document.getElementById("projects")
+const liveDemo = document.getElementById("live-demo")
+const projectGithub = document.getElementById("project-github")
+const submitBtn = document.getElementById("submit-btn")
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
@@ -51,4 +54,43 @@ gridCards3.addEventListener('click', function() {
 
 gridCards4.addEventListener('click', function() {
     sectionProject.scrollIntoView({behavior:'smooth'})
+})
+
+liveDemo.addEventListener('click', function() {
+    window.open("https://venkatasaisriram.github.io/Portfolio/", "_blank")
+})
+
+projectGithub.addEventListener('click', function() {
+    window.open('https://github.com/venkatasaisriram/Portfolio', '_blank')
+})
+
+//Pop Up
+
+const certImages = document.querySelectorAll('.cert-img');
+const modal = document.getElementById('cert-modal');
+const modalImg = document.getElementById('modal-img');
+const navBar = document.getElementById("nav-header")
+
+certImages.forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.src = img.src;
+    });
+});
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+const emailInput = document.getElementById("email-input")
+
+submitBtn.addEventListener('click', function() {
+    const email = emailInput.value.trim();
+
+    if(email) {
+        window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`, '_blank');
+    }
+    else {
+        alert("Please enter email address");
+    }
 })
